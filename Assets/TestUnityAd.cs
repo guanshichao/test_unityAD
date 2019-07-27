e5f6ede8daad5aaf533b8fc2be99ae995e4d2080
管世超 <guanshichao@conew.com>
2019-07-27 18:20:02 +0800

    测试代码、测试场景

---
+++ Assets/TestUnityAd.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Advertisements;
+public class TestUnityAd : MonoBehaviour {
+    private const string GAME_ID = "b055baa2-75b8-4c12-9ed6-3746cbc7de2f";
+
+    private void OnGUI()
+    {
+        if (Button("Init"))
+        {
+            Advertisement.Initialize(GAME_ID);
+        }
+        GUILayout.Label("inited:"+Advertisement.isInitialized,"box");
+        if (Button("chapin"))
+        {
+            Advertisement.Show("chapin_levelend");
+        }
+
+        if (Button("jili"))
+        {
+            Advertisement.Show("jili_levelend");
+        }
+    }
+
+    private static bool Button(string p)
+    {
+        return GUILayout.Button(p, GUILayout.Width(100), GUILayout.Height(100));
+    }
+}
